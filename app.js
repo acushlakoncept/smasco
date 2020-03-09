c47562f831b29a4139dabb7a0400acb2e8da9c7f
Uduak Essien <uduak.essien@ngonn.local>
2020-03-09 12:57:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const profileImg = document.getElementById('profileimg');
+const nameInput = document.getElementById('name');
+const yearInput = document.getElementById('grad');
+
+const imgPrev = document.getElementById('imgprev');
+const classof = document.querySelector('.year');
+const profileName = document.getElementById('profilename');
+
+const saveImg = document.getElementById('btnConvertHTML');
+const exportArea = document.getElementById('exportarea');
+
+
+
+profileImg.addEventListener('change', (event) => {
+    imgPrev.src = URL.createObjectURL(event.target.files[0]);
+})
+
+nameInput.addEventListener('keyup', (event) => {
+    profileName.textContent = event.target.value;
+})
+
+yearInput.addEventListener('keyup', (event) => {
+    classof.textContent = event.target.value;
+})
+
+let getCanvas;
+
+
+
+function takeShot() {
+    html2canvas(exportArea).then(function(canvas) {
+        document.querySelector('.preview__block').appendChild(canvas);
+        getCanvas = canvas;
+    });
+}
+
+
+// saveImg.addEventListener('click', takeShot);
+
+
+saveImg.addEventListener('click', event => {
+    html2canvas(exportArea, {
+        logging: true,
+        letterRendering: 1,
+        allowTaint: true,
+        useCORS: true,
+        width: 800,
+        height: 687
+    }).then(result => {
+        let img = result.toDataURL("image/png");
+        // let newData = img.replace(/^data:image\/png/, "data:application/octet-stream");
+        let newData = img.replace('image/png', "image/octet-stream");
+        // savePng(img);
+
+        let a = document.createElement('a');
+        a.href = newData;
+        a.download = 'smasco_pro.png';
+        a.click();
+    });
+})
